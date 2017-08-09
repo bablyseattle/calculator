ceea9eec703a5929115b515f71f3189500140049
Bably Kumari and Kelsey Langlois <kelsey@>
2017-08-09 11:10:21 -0700

    add form and user interface for

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,36 @@ var divide = function(number1, number2){
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+$(document).ready(function() {
+	$("#add").submit(function(event){
+		var number1 = parseInt($("input#add1").val());
+		var number2 = parseInt($("input#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+		event.preventDefault();
+	});
 
-alert(divide(number1, number2));
+	$("#subtract").submit(function(event) {
+		var number1 = parseInt($("input#subtract1").val());
+		var number2 = parseInt($("input#subtract2").val());
+		var result = subtract(number1, number2);
+		$("#output").text(result);
+		event.preventDefault();
+	});
+
+	$("#multiply").submit(function(event){
+		var number1 = parseInt($("input#multiply1").val());
+		var number2 = parseInt($("input#multiply2").val());
+		var result = multiply(number1,number2);
+		$("#output").text(result);
+		event.preventDefault();
+	});
+
+	$("#divide").submit(function(event){
+		var number1 = parseInt($("input#divide1").val());
+		var number2 = parseInt($("input#divide2").val());
+		var result = divide(number1,number2);
+		$("#output").text(result);
+		event.preventDefault();
+	});
+});
